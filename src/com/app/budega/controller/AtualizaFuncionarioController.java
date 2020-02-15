a8e4f2348eed6cedc3197049341991d7c8959b5f
GabrielT0maz <jgabrieltomaz2706@gmail.com>
2020-02-15 15:37:23 -0300

    Tratando as exceções da exclusão de funcionarios e exceções ao chamar a tela de atualizar.

--- src/com/app/budega/controller/AtualizaFuncionarioController.java
+++ src/com/app/budega/controller/AtualizaFuncionarioController.java
@@ -1,18 +1,19 @@
 package com.app.budega.controller;
 
+import com.app.budega.App.AtualizarFuncionarioMain;
 import com.app.budega.App.Main;
 import com.app.budega.dao.FuncionarioDAO;
 import com.app.budega.model.Funcionario;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class AtualizaFuncionarioController implements Initializable {
@@ -45,16 +46,47 @@ public class AtualizaFuncionarioController implements Initializable {
 
     @FXML
     void acaoAtualizar(ActionEvent event){
-        FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
-        String nome = campoNome.getText();
-        String senha = campoSenha1.getText();
-        try {
-            funcionarioDAO.atualizaFuncionario(nome,senha,funcionarioRetornado.getCpf());
-            ListarFuncionarioController listarFuncionarioController = new ListarFuncionarioController();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+        if(campoNome.getText().equals("") || campoSenha1.getText().equals("")||campoSenha2.getText().equals("")){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Atualização de Funcionario");
+            alert.setHeaderText("Todos os Campos com * devem ser preenchidos.");
+            alert.setContentText("Verifique os campos e tente novamente.");
+
+            alert.showAndWait();
+        }else if(!campoSenha2.getText().equals(campoSenha1.getText()) ){
+            campoSenha1.setStyle("-fx-border-color: #f44336");
+            campoSenha2.setStyle("-fx-border-color: #f44336");
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Atualização de Funcionario");
+            alert.setHeaderText("As senhas não batem.");
+            alert.setContentText("Verifique as senhas e tente novamente.");
+            alert.showAndWait();
+
+            campoSenha1.setStyle("-fx-border-color: #BDBDBD");
+            campoSenha2.setStyle("-fx-border-color: #BDBDBD");
+        }else {
+            FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
+            String nome = campoNome.getText();
+            String senha = campoSenha1.getText();
+            try {
+                funcionarioDAO.atualizaFuncionario(nome, senha, funcionarioRetornado.getCpf());
+                if(funcionarioDAO.atualizaFuncionario(nome, senha, funcionarioRetornado.getCpf()) == true){
+                   Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                   alert.setTitle("Atualização de Funcionario");
+                   alert.setHeaderText("Dados atualizado com sucesso.");
+                   alert.setContentText(labelCPF.getText());
+
+                    Optional <ButtonType> resultado = alert.showAndWait();
+                       if (resultado.get() == ButtonType.OK){
+                           Main main = new Main();
+                       }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
         }
     }
 
