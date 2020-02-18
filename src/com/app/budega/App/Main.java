84be9cf647a7f594288e362d83a0112394737fcd
GabrielT0maz <jgabrieltomaz2706@gmail.com>
2020-02-17 23:14:08 -0300

    Ajustando css do listar Fornecedor.

--- src/com/app/budega/App/Main.java
+++ src/com/app/budega/App/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("../view/fxml/ListarFornecedor.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/fxml/TelaLogin.fxml"));
         root.getStylesheets().add(getClass().getResource("../view/style/Main.css").toExternalForm());
         primaryStage.setTitle("Budega App");
         primaryStage.setScene(new Scene(root));
